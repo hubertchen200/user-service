05bfc4b8b875efb39341b7756c42934ad5debea6
Hubert Chen <hubertchen@Huberts-iMac.hsd1.nj.comcast.net>
2024-07-02 23:05:57 -0400

    remove comment

--- api/routes/jwt_routes/routes.py
+++ api/routes/jwt_routes/routes.py
@@ -5,7 +5,5 @@ from api.jwt_token.my_jwt import jwt_encode, jwt_decode
 def my_encode():
     payload = request.get_json()
     return jwt_encode(payload["user_id"], payload["username"])
-# @jwt_bp.route('/jwt/decode/<token>', methods = ['GET'])
-# def my_decode(token):
-#     return jwt_decode(token)
+
 
